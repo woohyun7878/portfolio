f03330712bde8cc7f67c35865441678448906f7d
Woohyun Jo <woohyun7878@gmail.com>
2021-04-02 22:11:25 +0900

    Website up to date.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-/*!
+    /*!
+    * Modifications made by Woohyun Jo
+    * Copyright 2021 Woohyun Jo
+    */
+    /*!
     * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
